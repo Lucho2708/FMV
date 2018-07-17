7e83e9979c1b941fac4c5bda9d7e13d79def801d
Hacero51 <zonaacero92@gmail.com>
2018-07-16 21:45:05 -0500

    cambios formularios 2.1

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -49,4 +49,47 @@
 
         </ul>
     </li>
+    <li class="{{ Request::is('cproceso') || Request::is('')? 'active' : null}}" >
+        <a href="javascript:void(0);" class="menu-toggle">
+            <i class="material-icons">assignment</i>
+            <span>Formularios</span>
+        </a>
+        <ul class="ml-menu">
+            <li class="{{ Request::is('cproceso') ? 'active' : null}}" >
+                <a href="{!!URL::to('/cproceso')!!}">Cierre de proceso</a>
+            </li>
+            <li class="{{ Request::is('diario') ? 'active' : null}}" >
+                <a href="{!!URL::to('/diario')!!}">Diario terapeutico</a>
+            </li>
+            <li class="{{ Request::is('hc') ? 'active' : null}}" >
+                <a href="{!!URL::to('/hc')!!}">Historia clinica</a>
+            </li>
+            <li class="{{ Request::is('sproceso') ? 'active' : null}}" >
+                <a href="{!!URL::to('/sproceso')!!}">Seguimiento de proceso</a>
+            </li>
+            <li class="{{ Request::is('rpsicologica') ? 'active' : null}}" >
+                <a href="{!!URL::to('/rpsicologica')!!}">Remision Psicologica</a>
+            </li>
+            <li class="{{ Request::is('hingreso') ? 'active' : null}}" >
+                <a href="{!!URL::to('/hingreso')!!}">Hoja de Ingreso</a>
+            </li>
+            <li class="{{ Request::is('eingreso') ? 'active' : null}}" >
+                <a href="{!!URL::to('/eingreso')!!}">Epicrisis de Ingreso</a>
+            </li>
+            <li class="{{ Request::is('eegreso') ? 'active' : null}}" >
+                <a href="{!!URL::to('/eegreso')!!}">Epicrisis de egreso</a>
+            </li>
+            <li class="{{ Request::is('eto') ? 'active' : null}}" >
+                <a href="{!!URL::to('/eto')!!}">Evaluacion terapia ocupacional</a>
+            </li>
+            <li class="{{ Request::is('encuesta') ? 'active' : null}}" >
+                <a href="{!!URL::to('/encuesta')!!}">Encuesta terapia ocupacional</a>
+            </li>
+            <li>
+                <a href="pages/forms/basic-form-elements.html">Basic Form Elements</a>
+            </li>
+        </ul>
+    </li>
+
 </ul>
+
